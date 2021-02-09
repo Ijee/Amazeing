dd5f8476c42086051c0b1d0562a8ef2d50bbfab6
Ijee <thorsten.schulz.ijee@googlemail.com>
2021-02-09 17:39:03 +0100

    layout changes

--- src/app/simulation/simulation.module.ts
+++ src/app/simulation/simulation.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {GridComponent} from './grid/grid.component';
 import {NodeComponent} from './grid/node/node.component';
 import {StatsComponent} from './grid/stats/stats.component';
 import {SimulationRoutingModule} from './simulation-routing.module';
 import {CoreModule} from '../@core/core.module';
-import { GridSettingsComponent } from './grid-settings/grid-settings.component';
 import {SimulationComponent} from './simulation.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {GridSettingsComponent} from './grid-settings/grid-settings.component';
+import {MazeSettingsComponent} from './grid-settings/maze-settings/maze-settings.component';
+import {PathfindingSettingsComponent} from './grid-settings/pathfinding-settings/pathfinding-settings.component';
 
 @NgModule({
-  declarations: [SimulationComponent, GridComponent, NodeComponent, StatsComponent, GridSettingsComponent],
+  declarations: [SimulationComponent, GridComponent, NodeComponent, StatsComponent,
+    GridSettingsComponent, MazeSettingsComponent, PathfindingSettingsComponent],
   imports: [
     CommonModule,
     SimulationRoutingModule,
@@ -19,4 +22,5 @@ import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
   ],
   providers: []
 })
-export class SimulationModule { }
+export class SimulationModule {
+}
