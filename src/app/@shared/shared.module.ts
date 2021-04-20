12030e5292e455a2e6a29eabcefecee8095bb72d
Ijee <thorsten.schulz.ijee@googlemail.com>
2021-04-20 14:39:28 +0200

    chore: refactor directives/pipes location

--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -3,12 +3,23 @@ import { CommonModule } from '@angular/common';
 import {HrComponent} from './components/hr/hr.component';
 import { WarningModalComponent } from './components/warning-modal/warning-modal.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {DropZoneDirective} from './directives/drop-zone.directive';
+import {CountAnimationDirective} from './directives/count-animation.directive';
+import {MaxNumberPipe} from './pipes/max-number.pipe';
 
 
 
 @NgModule({
-  declarations: [HrComponent, WarningModalComponent],
+  declarations: [
+    DropZoneDirective,
+    CountAnimationDirective,
+    MaxNumberPipe,
+    HrComponent,
+    WarningModalComponent],
   exports: [
+    DropZoneDirective,
+    CountAnimationDirective,
+    MaxNumberPipe,
     HrComponent,
     WarningModalComponent
   ],
