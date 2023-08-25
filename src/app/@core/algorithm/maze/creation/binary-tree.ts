ac4062fe4517b5e62cb2ed9af83bdda25bc67a21
ijee <ijeedev@gmail.com>
2023-08-25 15:42:22 +0200

    fix(algorithm): binary tree works as intended now for real maybe

--- src/app/@core/algorithm/maze/creation/binary-tree.ts
+++ src/app/@core/algorithm/maze/creation/binary-tree.ts
@@ -1,17 +1,13 @@
-import { MazeAlgorithm, Node, StatRecord } from 'src/app/@core/types/algorithm.types';
+import { Direction, MazeAlgorithm, Node, StatRecord } from 'src/app/@core/types/algorithm.types';
 import { GridLocation } from 'src/app/@shared/classes/GridLocation';
 import { MazeAlgorithmAbstract } from '../maze-algorithm.abstract';
 
 export class BinaryTree extends MazeAlgorithmAbstract {
     // Actually, we don't have any state for the algorithm itself.
     // This is only for the algorithm options or rather for the directional loop to be exact.
-    private buildDirection1: string;
-    private buildDirection2: string;
-    private xDirection: number;
-    private yDirection: number;
-    private xStart: number;
+    private direction1: Direction;
+    private direction2: Direction;
     private yStart: number;
-    private xEnd: number;
     private yEnd: number;
 
     constructor() {
@@ -43,12 +39,12 @@ export class BinaryTree extends MazeAlgorithmAbstract {
      * @param direction
      * @private
      */
-    private buildDirection(xAxis: number, yAxis: number, direction: string): void {
+    private buildDirection(xAxis: number, yAxis: number, direction: Direction): void {
         const loc = new GridLocation(xAxis, yAxis);
         this.buildWalls(loc, 0);
         this.currentGrid[xAxis][yAxis].status = 9;
         switch (direction) {
-            case 'top':
+            case 'up':
                 if (
                     this.currentGrid?.[xAxis]?.[yAxis - 1] !== undefined &&
                     this.currentGrid?.[xAxis]?.[yAxis - 1].status !== 2 &&
@@ -90,15 +86,16 @@ export class BinaryTree extends MazeAlgorithmAbstract {
     }
 
     public nextStep(): Node[][] {
-        for (let i = this.yStart; i !== this.yEnd; i += this.yDirection) {
-            for (let j = this.xStart; j !== this.xEnd; j += this.xDirection) {
+        for (let i = this.yStart; i < this.yEnd; i++) {
+            for (let j = 0; j < this.currentGrid.length; j++) {
                 if (this.currentGrid[j][i].status === 0) {
                     let coinFlip = Math.floor(Math.random() * 2) + 1;
+
                     if (coinFlip === 1) {
-                        this.buildDirection(j, i, this.buildDirection1);
+                        this.buildDirection(j, i, this.direction1);
                         return this.currentGrid;
                     } else {
-                        this.buildDirection(j, i, this.buildDirection2);
+                        this.buildDirection(j, i, this.direction2);
                         return this.currentGrid;
                     }
                 }
@@ -112,44 +109,28 @@ export class BinaryTree extends MazeAlgorithmAbstract {
         // Settings the options once.
         switch (this.options.Bias) {
             case 'Northwest':
-                this.buildDirection1 = 'right';
-                this.buildDirection2 = 'down';
-                this.xDirection = 1;
-                this.yDirection = 1;
-                this.xStart = 0;
-                this.yStart = 0;
-                this.xEnd = this.currentGrid.length;
+                this.direction1 = 'up';
+                this.direction2 = 'left';
+                this.yStart = 2;
                 this.yEnd = this.currentGrid[0].length;
                 break;
             case 'Northeast':
-                this.buildDirection1 = 'left';
-                this.buildDirection2 = 'down';
-                this.xDirection = -1;
-                this.yDirection = 1;
-                this.xStart = this.currentGrid.length;
-                this.yStart = 0;
-                this.xEnd = 0;
+                this.direction1 = 'up';
+                this.direction2 = 'right';
+                this.yStart = 2;
                 this.yEnd = this.currentGrid[0].length;
                 break;
             case 'Southwest':
-                this.buildDirection1 = 'right';
-                this.buildDirection2 = 'top';
-                this.xDirection = 1;
-                this.yDirection = -1;
-                this.xStart = 0;
-                this.yStart = this.currentGrid[0].length - 1;
-                this.xEnd = this.currentGrid.length;
-                this.yEnd = -1;
+                this.direction1 = 'down';
+                this.direction2 = 'left';
+                this.yStart = 0;
+                this.yEnd = this.currentGrid[0].length - 1;
                 break;
             case 'Southeast':
-                this.buildDirection1 = 'left';
-                this.buildDirection2 = 'top';
-                this.xDirection = -1;
-                this.yDirection = -1;
-                this.xStart = this.currentGrid.length - 1;
-                this.yStart = this.currentGrid[0].length - 1;
-                this.xEnd = -1;
-                this.yEnd = -1;
+                this.direction1 = 'down';
+                this.direction2 = 'right';
+                this.yStart = 0;
+                this.yEnd = this.currentGrid[0].length - 1;
                 break;
             default:
         }
@@ -162,53 +143,29 @@ export class BinaryTree extends MazeAlgorithmAbstract {
     ): void {
         this.currentGrid = newGrid;
         this.statRecords = statRecords;
-        this.buildDirection1 = deserializedState.buildDirection1;
-        this.buildDirection2 = deserializedState.buildDirection2;
-        this.xDirection = deserializedState.xDirection;
-        this.yDirection = deserializedState.yDirection;
-        this.xStart = deserializedState.xStart;
-        this.yStart = deserializedState.yStart;
-        this.xEnd = deserializedState.xEnd;
-        this.yEnd = deserializedState.yEnd;
+        this.direction1 = deserializedState.direction1;
+        this.direction2 = deserializedState.direction2;
     }
 
     public deserialize(newGrid: Node[][], serializedState: any, statRecords: StatRecord[]): void {
         const deserializedState = {
-            buildDirection1: serializedState.buildDirection1,
-            buildDirection2: serializedState.buildDirection2,
-            xDirection: serializedState.xDirection,
-            yDirection: serializedState.yDirection,
-            xStart: serializedState.xStart,
-            yStart: serializedState.yStart,
-            xEnd: serializedState.xEnd,
-            yEnd: serializedState.yEnd
+            buildDirection1: serializedState.direction1,
+            buildDirection2: serializedState.direction2
         };
         this.updateAlgorithmState(newGrid, deserializedState, statRecords);
     }
 
     public getSerializedState(): Object {
         return {
-            buildDirection1: this.buildDirection1,
-            buildDirection2: this.buildDirection2,
-            xDirection: this.xDirection,
-            yDirection: this.yDirection,
-            xStart: this.xStart,
-            yStart: this.yStart,
-            xEnd: this.xEnd,
-            yEnd: this.yEnd
+            buildDirection1: this.direction1,
+            buildDirection2: this.direction2
         };
     }
 
     public getCurrentAlgorithmState(): Object {
         return {
-            buildDirection1: this.buildDirection1,
-            buildDirection2: this.buildDirection2,
-            xDirection: this.xDirection,
-            yDirection: this.yDirection,
-            xStart: this.xStart,
-            yStart: this.yStart,
-            xEnd: this.xEnd,
-            yEnd: this.yEnd
+            buildDirection1: this.direction1,
+            buildDirection2: this.direction2
         };
     }
 
