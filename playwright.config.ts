c0cf0ce191987319a57aa1769a2f61791e25e668
ijee <15277745+Ijee@users.noreply.github.com>
2024-03-25 19:47:13 +0100

    fix(ci): changed playwright tests action + added artifact upload

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
-        screenshot: 'on'
+        screenshot: 'only-on-failure'
     },
 
     /* Configure projects for major browsers */
@@ -65,7 +65,7 @@ export default defineConfig({
     ],
 
     webServer: {
-        command: ' ng serve',
+        command: 'ng serve',
         url: 'http://localhost:4200',
         reuseExistingServer: true,
         timeout: 60000
