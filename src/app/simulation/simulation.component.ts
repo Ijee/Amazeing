35efcc48377277532dbabc9a02dc618dda8ba004
ijee <15277745+Ijee@users.noreply.github.com>
2024-05-04 11:00:02 +0200

    fix: adjust app paddings

--- src/app/simulation/simulation.component.ts
+++ src/app/simulation/simulation.component.ts
@@ -3,6 +3,8 @@ import { fadeAnimationSafe } from '../@shared/animations/fadeRouteAnimation';
 import { ControllerComponent } from './controller/controller.component';
 import { GridSettingsComponent } from './grid-settings/grid-settings.component';
 import { GridComponent } from './grid/grid.component';
+import { BreakpointService } from '../@core/services/breakpoint.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-simulation',
@@ -10,8 +12,10 @@ import { GridComponent } from './grid/grid.component';
     styleUrls: ['./simulation.component.scss'],
     animations: [fadeAnimationSafe],
     standalone: true,
-    imports: [GridComponent, GridSettingsComponent, ControllerComponent]
+    imports: [CommonModule, GridComponent, GridSettingsComponent, ControllerComponent]
 })
 export class SimulationComponent {
-    constructor() {}
+    // TOOD: component paddings are messed up and need adjustments.
+    // breakpointservice shouldn't be necessary
+    constructor(public readonly breakpointService: BreakpointService) {}
 }
