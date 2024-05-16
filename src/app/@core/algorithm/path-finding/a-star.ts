9a23e71217d368effaf7501bac6fbbeafdae0eb1
ijee <15277745+Ijee@users.noreply.github.com>
2024-05-16 23:46:41 +0200

    feat: add first working a-star algorithm version
    chore: wip
    
    chore: wip

--- src/app/@core/algorithm/path-finding/a-star.ts
+++ src/app/@core/algorithm/path-finding/a-star.ts
@@ -1,57 +1,248 @@
 import { PathFindingAlgorithmAbstract } from './path-finding-algorithm.abstract';
 import { GridLocation } from '../../../@shared/classes/GridLocation';
-import { Node, PathFindingAlgorithm, Statistic } from '../../types/algorithm.types';
-import { JsonFormData } from '../../types/jsonform.types';
+import { Node, PathFindingAlgorithm, Statistic, VisitedNode } from '../../types/algorithm.types';
+import { PriorityQueue } from 'src/app/@shared/classes/PriorityQueue';
+import { HashMap } from 'src/app/@shared/classes/HasMap';
 
 export class AStar extends PathFindingAlgorithmAbstract {
+    private priorityQueue: PriorityQueue;
+    private visitedNodes: HashMap<GridLocation, VisitedNode>;
+    private currentPath: GridLocation[];
+    private tracePath: GridLocation;
     constructor() {
-        super([], [], {
-            controls: []
-        });
+        super(
+            [],
+            [
+                {
+                    name: 'Queued',
+                    type: 'status-4'
+                },
+                {
+                    name: 'Visited',
+                    type: 'status-5'
+                },
+                {
+                    name: 'Fastest Path',
+                    type: 'status-8'
+                },
+                {
+                    name: 'Current Path (optional)',
+                    type: 'status-6'
+                }
+            ],
+            {
+                controls: [
+                    {
+                        name: 'Show current path',
+                        label: 'Show current path',
+                        value: '',
+                        type: 'checkbox',
+                        validators: {
+                            required: true
+                        }
+                    }
+                ]
+            }
+        );
+        this.priorityQueue = new PriorityQueue();
+        this.visitedNodes = new HashMap<GridLocation, VisitedNode>();
+        this.currentPath = [];
+    }
+
+    /**
+     * Calculates whether a better path is available for a neighbour node.
+     *
+     * @param currentNode the current node
+     * @param neighbourNode the neighbour node
+     */
+    private relaxation(currentNode: GridLocation, neighbourNode: GridLocation): void {
+        const currentVisitedNode = this.visitedNodes.get(currentNode);
+        const neighborVisitedNode = this.visitedNodes.get(neighbourNode);
+
+        // Calculate tentative g-cost
+        const tentativeGCost = currentVisitedNode.distance + neighbourNode.weight;
+
+        if (tentativeGCost < neighborVisitedNode.distance) {
+            // Compare tentative g-cost with the current distance
+            const heuristicCost = this.calculateHeuristic(neighbourNode);
+            const totalCost = tentativeGCost + heuristicCost; // Total cost is g(n) + h(n)
+
+            const updatedVisitedNode: VisitedNode = {
+                predecessor: currentNode,
+                distance: tentativeGCost
+            };
+
+            this.visitedNodes.put(neighbourNode, updatedVisitedNode);
+            this.grid[neighbourNode.x][neighbourNode.y].text = tentativeGCost.toString();
+
+            // Update the priority queue with the total cost (g + h)
+            this.priorityQueue.update(neighbourNode, totalCost);
+        }
     }
 
     public nextStep(): Node[][] {
-        throw new Error('Method not implemented.');
+        // Paints the current path back
+        for (var i = 0; i < this.currentPath.length; i++) {
+            const node = this.currentPath[i];
+            this.paintNode(node.x, node.y, 5);
+        }
+        this.currentPath = [];
+
+        if (!this.priorityQueue.isEmpty()) {
+            const currLoc = this.priorityQueue.dequeue();
+            if (this.grid[currLoc.x][currLoc.y].status === 3) {
+                this.tracePath = currLoc;
+                this.priorityQueue.empty();
+                return this.grid;
+            }
+            this.paintNode(currLoc.x, currLoc.y, 5);
+            if (this.options['Show current path']) {
+                let pathNode = currLoc;
+                while (pathNode) {
+                    if (pathNode.status === 2) {
+                        pathNode = undefined;
+                    } else {
+                        pathNode = this.visitedNodes.get(pathNode).predecessor;
+                        this.currentPath.push(pathNode);
+                        this.paintNode(pathNode.x, pathNode.y, 6);
+                    }
+                }
+            }
+            const currentVisitedNode = this.visitedNodes.get(currLoc);
+            const neighbours = this.getNeighbours(currLoc, 1).filter((neighbour) => {
+                return neighbour.status !== 1;
+            });
+            neighbours.forEach((neighbour) => {
+                if (!this.visitedNodes.contains(neighbour)) {
+                    const distance =
+                        currentVisitedNode.distance +
+                        neighbour.weight +
+                        this.calculateHeuristic(neighbour);
+                    const visitedNode: VisitedNode = {
+                        predecessor: currLoc,
+                        distance: distance
+                    };
+                    this.grid[neighbour.x][neighbour.y].text = distance.toString();
+                    this.visitedNodes.put(neighbour, visitedNode);
+                    this.priorityQueue.enqueue(neighbour, distance);
+                    // Mark as queued
+                    this.paintNode(neighbour.x, neighbour.y, 4);
+                } else {
+                    this.relaxation(currLoc, neighbour);
+                }
+            });
+        } else if (this.priorityQueue.isEmpty() && !this.tracePath) {
+            // No path to goal exists.
+            return null;
+        } else if (this.tracePath) {
+            // Backtracking to show the path
+            this.paintNode(this.tracePath.x, this.tracePath.y, 8); // Paint the path
+            this.tracePath = this.visitedNodes.get(this.tracePath).predecessor || undefined;
+            if (this.grid[this.tracePath.x][this.tracePath.y].status === 2) {
+                this.tracePath = null;
+            }
+        }
+
+        return this.grid;
     }
 
     public setInitialData(grid: Node[][], startLocation: GridLocation): void {
-        throw new Error('Method not implemented.');
-    }
+        this.grid = grid;
 
-    public setOptions(options: Object): void {
-        throw new Error('Method not implemented.');
+        this.priorityQueue.enqueue(startLocation, 0);
+        this.visitedNodes.put(startLocation, { predecessor: startLocation, distance: 0 });
+
+        // initial distances (excluding walls)
+
+        for (let i = 0; i < this.grid.length; i++) {
+            for (let j = 0; j < this.grid[0].length; j++) {
+                let node = this.grid[i][j];
+                if (this.grid[i][j].status !== 2 && this.grid[i][j].status !== 1) {
+                    node.text = '∞';
+                } else if (this.grid[i][j].status === 2) {
+                    node.text = '0';
+                    node.weight = 0;
+                }
+            }
+        }
     }
 
     public updateState(newGrid: Node[][], deserializedState: any, statRecords: Statistic[]): void {
-        throw new Error('Method not implemented.');
+        this.grid = newGrid;
+        this.statRecords = statRecords;
+
+        this.priorityQueue = deserializedState.priorityQueue;
+        this.visitedNodes = deserializedState.visitedNodes;
+        this.currentPath = deserializedState.currentPath;
+        this.tracePath = deserializedState.tracePath;
     }
 
     public deserialize(newGrid: Node[][], serializedState: any, statRecords: Statistic[]): void {
-        throw new Error('Method not implemented.');
+        const tracePath = serializedState.tracePath;
+        const deserializedState = {
+            priorityQueue: new PriorityQueue(),
+            visitedNodes: new HashMap<GridLocation, GridLocation>(),
+            tracePath: new GridLocation(
+                tracePath.x,
+                tracePath.y,
+                tracePath.weight,
+                tracePath.status
+            )
+        };
+        serializedState.priorityQueue.forEach((element) => {
+            const node = element.node;
+            const loc = new GridLocation(node.x, node.y, node.weight, node.status);
+            deserializedState.priorityQueue.enqueue(loc, element.priority);
+        });
+
+        serializedState.visitedNodes.forEach((ele) => {
+            deserializedState.visitedNodes.put(ele.key, ele.value);
+        });
+        for (var i = 0; i < serializedState.currentPath.length; i++) {
+            const node = serializedState.currentPath[i];
+            serializedState.currentPath.push(
+                new GridLocation(node.x, node.y, node.weight, node.status)
+            );
+        }
+
+        this.updateState(newGrid, deserializedState, statRecords);
     }
 
     public serialize(): Object {
-        throw new Error('Method not implemented.');
+        const serializedState = {
+            priorityQueue: this.priorityQueue.toObject(),
+            visitedNodes: [],
+            currentPath: [],
+            tracePath: this.tracePath.toObject()
+        };
+
+        this.visitedNodes.forEach((ele) => {
+            const entry = { key: ele.toObject(), value: this.visitedNodes.get(ele) };
+            serializedState.visitedNodes.push(entry);
+        });
+        for (var i = 0; i < this.currentPath.length; i++) {
+            const node = this.currentPath[i];
+            serializedState.currentPath.push(node.toObject());
+        }
+
+        return serializedState;
     }
 
     public getState(): Object {
-        throw new Error('Method not implemented.');
+        return {
+            priorityQueue: this.priorityQueue,
+            visitedNodes: this.visitedNodes,
+            currentPath: this.currentPath,
+            tracePath: this.tracePath
+        };
     }
 
     public getAlgorithmName(): PathFindingAlgorithm {
         return 'A-Star';
     }
 
-    public getStatRecords(): Statistic[] {
-        throw new Error('Method not implemented.');
-    }
-
-    public getJsonFormData(): JsonFormData {
-        throw new Error('Method not implemented.');
-    }
-
     public usesNodeWeights(): boolean {
-        throw new Error('Method not implemented.');
+        return true;
     }
 
     public usesHeuristics(): boolean {
