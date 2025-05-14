d7e3f87064ad069208912b4a722ea06496326480
ijee <15277745+Ijee@users.noreply.github.com>
2025-05-14 08:34:31 +0200

    fix(algorithm): celullar automaton now correctly appllies the rules
    Pretty embarrassing—shows how little patience I had left to finish this last year.

--- src/app/@core/algorithm/maze/creation/cellular-automaton.ts
+++ src/app/@core/algorithm/maze/creation/cellular-automaton.ts
@@ -4,20 +4,30 @@ import { MazeAlgorithmAbstract } from '../maze-algorithm.abstract';
 
 export class CellularAutomaton extends MazeAlgorithmAbstract {
     constructor() {
-        super([], [], {
-            controls: [
+        super(
+            [],
+            [
                 {
-                    name: 'Ruleset',
-                    label: 'Ruleset',
-                    value: 'Mazectric Ruleset (B3/S1234)',
-                    values: ['Mazectric Ruleset (B3/S1234)', 'Maze Ruleset (B3/S12345)'],
-                    type: 'radio',
-                    validators: {
-                        required: true
-                    }
+                    name: 'Alive',
+                    type: 'status-1',
+                    currentValue: 0
                 }
-            ]
-        });
+            ],
+            {
+                controls: [
+                    {
+                        name: 'Ruleset',
+                        label: 'Ruleset',
+                        value: 'Mazectric Ruleset (B3/S1234)',
+                        values: ['Mazectric Ruleset (B3/S1234)', 'Maze Ruleset (B3/S12345)'],
+                        type: 'radio',
+                        validators: {
+                            required: true
+                        }
+                    }
+                ]
+            }
+        );
     }
 
     protected override getNeighbours(loc: GridLocation): GridLocation[] {
@@ -53,62 +63,79 @@ export class CellularAutomaton extends MazeAlgorithmAbstract {
     /**
      *  Applies the celullar automaton Mazectric ruleset (B3/S1234)
      *
-     * @param loc the location to apply the ruleset to
+     * @param currentStatus the current status of the cell
      * @param aliveNeighbours the amount of alive cell neighbours
+     *
+     * @returns 1 if the cell is alive; 0 if it is dead
      */
-    private applyMazectricRuleset(loc: GridLocation, aliveNeighbours: number): void {
+    private applyMazectricRuleset(currentStatus: number, aliveNeighbours: number): number {
         // Rule nr. 1: A dead cell becomes alive if it has exactly 3 live neighbors.
-        if (this.grid[loc.x][loc.y].status === 0 && aliveNeighbours === 3) {
-            this.paintNode(loc.x, loc.y, 1);
-        } // Rule nr. 2:  A live cell survives from one generation to the next if they
-        //                have at least one and at most four alive neighbours.
-        if (this.grid[loc.x][loc.y].status === 1 && aliveNeighbours <= 4) {
-            this.paintNode(loc.x, loc.y, 1);
+        if (currentStatus === 0 && aliveNeighbours === 3) {
+            return 1;
         }
-        if (this.grid[loc.x][loc.y].status === 1 && aliveNeighbours > 4) {
-            // Rule nr. 3: If it has more than 4 alive neighbour cells it does not surive.
-            this.paintNode(loc.x, loc.y, 0);
+        // Rule nr. 2: A live cell survives if it has 1, 2, 3, or 4 alive neighbours.
+        if (currentStatus === 1 && aliveNeighbours >= 1 && aliveNeighbours <= 4) {
+            return 1;
         }
+        // In all other cases it becomes or remains dead.
+        return 0;
     }
 
     /**
      *  Applies the Maze ruleset (B3/S12345)
      *
-     * @param loc the location to apply the ruleset to
+     * @param currentStatus the current status of the cell
      * @param aliveNeighbours the amount of alive cell neighbours
+     *
+     *  @returns 1 if the cell is alive; 0 if it is dead
      */
-    private applyMazeRuleset(loc: GridLocation, aliveNeighbours: number): void {
+    private applyMazeRuleset(currentStatus: number, aliveNeighbours: number): number {
         // Rule nr. 1: A dead cell becomes alive if it has exactly 3 live neighbors.
-        if (this.grid[loc.x][loc.y].status === 0 && aliveNeighbours === 3) {
-            this.paintNode(loc.x, loc.y, 1);
-        } // Rule nr. 2:  A live cell survives from one generation to the next if they
-        //                have at least one and at most five alive neighbours.
-        if (this.grid[loc.x][loc.y].status === 1 && aliveNeighbours <= 5) {
-            this.paintNode(loc.x, loc.y, 1);
+        if (currentStatus === 0 && aliveNeighbours === 3) {
+            return 1;
         }
-        if (this.grid[loc.x][loc.y].status === 1 && aliveNeighbours > 5) {
-            // Rule nr. 3: If it has more than 5 alive neighbour cells it does not surive.
-            this.paintNode(loc.x, loc.y, 0);
+        // Rule nr. 2: A live cell survives if it has 1, 2, 3, 4, or 5 alive neighbours.
+        if (currentStatus === 1 && aliveNeighbours >= 1 && aliveNeighbours <= 5) {
+            return 1;
         }
+        // In all other cases it becomes or remains dead.
+        return 0;
     }
 
     public nextStep(): Node[][] {
+        // So that we do not change the original grid.
+        const nodeChanges: GridLocation[] = [];
+
         for (let i = 0; i < this.grid.length; i++) {
             for (let j = 0; j < this.grid[0].length; j++) {
-                const node = new GridLocation(i, j, undefined, this.grid[i][j].status);
-                const aliveNeighbors = this.getNeighbours(node).reduce(
+                const currentNode = this.grid[i][j];
+                const loc = new GridLocation(i, j, undefined, currentNode.status);
+                const aliveNeighbors = this.getNeighbours(loc).reduce(
                     (count, neighbor) => count + (neighbor.status === 1 ? 1 : 0),
                     0
                 );
-                if (this.options['Ruleset'] === 'Mazectric Ruleset (B3/S1234') {
-                    this.applyMazectricRuleset(node, aliveNeighbors);
+
+                let nextStatus: number;
+
+                if (this.options['Ruleset'] === 'Mazectric Ruleset (B3/S1234)') {
+                    nextStatus = this.applyMazectricRuleset(currentNode.status, aliveNeighbors);
                 } else {
-                    this.applyMazeRuleset(node, aliveNeighbors);
+                    nextStatus = this.applyMazeRuleset(currentNode.status, aliveNeighbors);
+                }
+
+                if (currentNode.status !== nextStatus) {
+                    nodeChanges.push(new GridLocation(i, j, undefined, nextStatus));
                 }
             }
         }
+
+        for (const change of nodeChanges) {
+            this.paintNode(change.x, change.y, change.status);
+        }
+
         return this.grid;
     }
+
     public setInitialData(grid: Node[][]): void {
         this.grid = grid;
     }
@@ -117,15 +144,19 @@ export class CellularAutomaton extends MazeAlgorithmAbstract {
         this.grid = newGrid;
         this.statRecords = statRecords;
     }
+
     public deserialize(newGrid: Node[][], serializedState: any, statRecords: Statistic[]): void {
         this.updateState(newGrid, {}, statRecords);
     }
+
     public serialize(): object {
         return {};
     }
+
     public getState(): object {
         return {};
     }
+
     public getAlgorithmName(): MazeAlgorithm {
         return 'Cellular-Automaton';
     }
