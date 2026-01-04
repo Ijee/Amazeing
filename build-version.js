fd63b3610f213e61e9c9f7b3d51d2956aa73b98e
ijee <15277745+Ijee@users.noreply.github.com>
2026-01-04 13:48:45 +0100

    ci: fix app version build script

--- build-version.js
+++ build-version.js
@@ -1,32 +1,23 @@
 import { execSync } from 'child_process';
 import { writeFileSync } from 'fs';
-import { config } from 'dotenv';
 
 try {
-    // Fetch the current commit hash.
-    const commitHash = execSync('git rev-parse HEAD').toString().trim();
+    // Fetch the current commit hash. (not needed for HEAD tag just in case I need it again)
+    // const commitHash = execSync('git rev-parse HEAD').toString().trim();
     // Fetch the Git tag associated with the current commit.
-    const version = execSync(`git describe --tags --always ${commitHash}`).toString().trim();
+    const version = execSync('git describe --tags --always').toString().trim();
 
-    writeFileSync('.env', `VERSION=${version}\n`);
-    console.log(`VERSION=${version}`);
-} catch (error) {
-    console.error('Error fetching Git tag:', error);
-    process.exit(1);
-}
+    const targetPath = './src/environments/environment.prod.ts';
 
-config();
-
-const targetPath = './src/environments/environment.prod.ts';
-
-const version = process.env.VERSION || 'unknown';
-
-const envConfigFile = `
+    const envConfigFile = `
 export const environment = {
   production: true,
   version: '${version}'
-};
-`;
+};`;
 
-writeFileSync(targetPath, envConfigFile);
-console.log(`Environment file generated at ${targetPath}`);
+    writeFileSync(targetPath, envConfigFile);
+    console.log(`Environment file generated at ${targetPath} with version: ${version}`);
+} catch (error) {
+    console.error('Error generating environment file: ', error);
+    process.exit(1);
+}
